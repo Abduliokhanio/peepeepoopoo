828cb0b30421767fe2fceff85d1aeba8f43d8f05
Johnathan Chen <johnathan@mydocspace.com>
2022-10-05 00:56:35 -0500

    account page

--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -1,8 +1,9 @@
 import React, { useRef, useState } from 'react';
+import Navbar from '../components/Navbar';
 import { supabase } from '../services/supabaseClient';
 import { useNavigate, Link } from 'react-router-dom';
 import {
-  Heading, Image, useToast, Text, FormControl, Flex, Stack, InputLeftElement, InputGroup, Box, Input, InputRightElement, Button
+  Heading, HStack, useToast, Text, FormControl, Flex, Stack, InputLeftElement, InputGroup, Box, Input, InputRightElement, Button
 } from '@chakra-ui/react';
 import { Icon, ChevronRightIcon } from '@chakra-ui/icons';
 import { MdOutlineReceiptLong, MdOutlineAccountCircle } from 'react-icons/md';
@@ -17,12 +18,19 @@ export default function AccountPage() {
 
   return (
     <Stack backgroundColor="#F9FBFC;" direction="column" minH="100vh" h="100%">
-      <Heading>Your account</Heading>
-      <Flex>
-        <Box>
-          <Icon as={MdOutlineAccountCircle} />
-          <Text>Edit account details</Text>
-        </Box>
+      <Navbar title="Your account" showBackButton={true} brandColor={localStorage.getItem('brandColor')} />
+      <Flex borderBottom='1px' borderColor='gray.200' px="6" py="6" justifyContent="space-between">
+        <HStack spacing="4">
+          <Icon fontSize="xl" as={MdOutlineAccountCircle} />
+          <Text fontSize="xl">Edit account details</Text>
+        </HStack>
+        <ChevronRightIcon />
+      </Flex>
+      <Flex borderBottom='1px' borderColor='gray.200' px="6" py="6" justifyContent="space-between">
+        <HStack spacing="4">
+          <Icon fontSize="xl" as={MdOutlineReceiptLong} />
+          <Text fontSize="xl">Orders & reciepts</Text>
+        </HStack>
         <ChevronRightIcon />
       </Flex>
     </Stack>
