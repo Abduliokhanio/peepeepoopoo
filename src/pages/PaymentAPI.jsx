5a7994c69f24fbf4ce96f40a795274da626bf098
Johnathan Chen <johnathan@mydocspace.com>
2022-11-03 02:08:48 -0500

    working payment

--- src/pages/PaymentAPI.jsx
+++ src/pages/PaymentAPI.jsx
@@ -13,45 +13,51 @@ export default function PostPayment() {
     postPayment();
   }, [false]);
 
+  function jsonToQueryString(json) {
+    return '?' + 
+        Object.keys(json).map(function(key) {
+          return encodeURIComponent(key) + '=' +
+                encodeURIComponent(json[key]);
+        }).join('&');
+  }
+
   const data = {
     'type': 'sale',
-    'amount': 100.00,
+    'amount': 102303.00,
     'ccnumber': 4111111111111111,
     'ccexp': 1025,
     'cvv': 999,
-    'first_name': 'Test',
-    'last_name': 'User',
-    'address1': '888 Main St',
-    'city': 'New York',
-    'state': 'NY',
-    'zip' : '77777',
-    'shipping_first_name': 'User',
-    'shipping_last_name': 'Test',
-    'shipping_address1': '987 State St',
-    'shipping_city': 'Los Angeles',
-    'shipping_state': 'CA',
-    'shipping_zip' : '98765',
-    'sercurity_key': Skey
+    // 'first_name': 'Test',
+    // 'last_name': 'User',
+    // 'address1': '888 Main St',
+    // 'city': 'New York',
+    // 'state': 'NY',
+    // 'zip' : '77777',
+    // 'shipping_first_name': 'User',
+    // 'shipping_last_name': 'Test',
+    // 'shipping_address1': '987 State St',
+    // 'shipping_city': 'Los Angeles',
+    // 'shipping_state': 'CA',
+    // 'shipping_zip' : '98765',
+    'security_key': Skey
   };
 
   const postPayment = async () => {
-    const requestOptions = {
-      mode: 'no-cors',
-      method: 'POST',
-      headers: { 
-        'Content-Type': 'application/json',
-        'Content-Length': data.length
-      },
-      body: data
+
+    const options = {
+      method: 'POST'
     };
 
-    fetch('https://sharingthecredit.transactiongateway.com/api/transact.php', requestOptions)
+    fetch(`https://cors-anywhere.herokuapp.com/https://sharingthecredit.transactiongateway.com/api/transact.php${jsonToQueryString(data)}`, options)
       .then(response => {
-        console.log(response);
-        setResponse(JSON.stringify(response));
+        response.text().then((s) => {
+          console.log(s);
+          setResponse(s);
+        });
+
       })
       .then((data, err) => {
-        console.log(data);
+        console.log(JSON.stringify(data));
         console.log(err);
         setResData(resData);
       });   
