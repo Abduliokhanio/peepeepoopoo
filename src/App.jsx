1919bcb2bba3a7e5eb3a4ef270f4f73dcef6005d
Johnathan Chen <johnathan@mydocspace.com>
2022-11-17 14:02:17 -0600

    another

--- src/App.jsx
+++ src/App.jsx
@@ -27,28 +27,28 @@ function App() {
       <BrowserRouter>
         <div className="App">
           <AuthProvider>
-          <Routes>
-            {/* <Route path="/" /> */}
-            <Route path="/:merchant" element={<Categories />} />
-            {/* <Route path="/:merchant/table/*" element={<Categories />} /> */}
-            <Route path="products" element={<Products />} />
-            <Route path="modifiers" element={<Modifiers />} />
-            <Route path="404" element={<PageNotFound />} />
-            <Route path="/user/account" element={<Account />} />
-            <Route path="/auth/signup" element={<Signup />} />
-            <Route path="/auth/verify" element={<Verify />} />
+            <Routes>
+              <Route exact path="/" />
+              <Route path="/:merchant" element={<Categories />} />
+              <Route path="/:merchant/table/*" element={<Categories />} />
+              <Route path="products" element={<Products />} />
+              <Route path="modifiers" element={<Modifiers />} />
+              <Route path="404" element={<PageNotFound />} />
+              <Route path="/user/account" element={<Account />} />
+              <Route path="/auth/signup" element={<Signup />} />
+              <Route path="/auth/verify" element={<Verify />} />
              
-            {/* <Route path='/user/new-card' element={<AddCardPayment />} />
+              {/* <Route path='/user/new-card' element={<AddCardPayment />} />
               <Route path='/user/payment-methods' element={<PaymentMethods />} />
               <Route path='/user/account-details' element={<AccountDetails />} />
               <Route path="/user/orders" element={<Orders />} />
               <Route path="/cart/checkout" element={<Checkout />} />
               <Route path="/cart/tips" element={<Tips />} />
               <Route path="/cart/order-confirmed" element={<OrderConfirmed />} /> */}
-            <Route element={<PrivateRoute />}>
-              <Route path="/cart/closed-tab" element={<ClosedTab />} />
-            </Route>   
-          </Routes>
+              <Route element={<PrivateRoute />}>
+                <Route path="/cart/closed-tab" element={<ClosedTab />} />
+              </Route>   
+            </Routes>
           </AuthProvider>
         </div>
       </BrowserRouter>         
