499194e9de36e6e2164f57b3aebb52e0524b883d
Johnathan Chen <johnathan@mydocspace.com>
2023-01-06 05:39:32 -0600

    env

--- cypress.config.js
+++ cypress.config.js
@@ -3,8 +3,8 @@ const { defineConfig } = require('cypress');
 module.exports = defineConfig({
   projectId: 'd711g3',
   env: {
-    PERCY_TOKEN: Cypress.env('percy_token'),
-    CYPRESS_RECORD_KEY: Cypress.env('cypress_record_key')
+    PERCY_TOKEN: 'c5336ebef23a5d9ac53f7a6f30255eb10c445cd785144546eca58cc82fea8587',
+    CYPRESS_RECORD_KEY: '018a4f80-d08f-450d-bfaf-7a327aedf281'
   },
   e2e: {
     baseUrl: 'http://localhost:3000'
