cd290d285de2f3ad03e050aeca4b645179d50504
Johnathan Chen <johnathan@mydocspace.com>
2023-01-06 04:00:42 -0600

    project id in e2e folder

--- cypress.config.js
+++ cypress.config.js
@@ -1,12 +1,10 @@
 const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
-  projectId: 'd711g3',
-
   e2e: {
+    projectId: 'd711g3',
     baseUrl: 'http://localhost:3000'
   },
-
   component: {
     devServer: {
       framework: 'create-react-app',
--- cypress/e2e/visual/visualTesting.cy.js
+++ cypress/e2e/visual/visualTesting.cy.js
@@ -1,6 +1,6 @@
 describe('Integration test with visual testing', function () {
   it('Loads digital menu categories', function () {
-    cy.visit('http://localhost:3000/nu-wood-fire-grill');
+    cy.visit('/nu-wood-fire-grill');
     cy.contains('Appetizers').then(() => {
       cy.percySnapshot('Digital menu test', {
         widths: [320, 375, 414]
