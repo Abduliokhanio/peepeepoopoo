dd8796e986df6c1df786a5a6fa33b41713cc8a7a
Johnathan Chen <johnathan@mydocspace.com>
2023-01-06 06:19:17 -0600

    update cypress config file

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,10 @@ const { defineConfig } = require('cypress');
 module.exports = defineConfig({
   projectId: 'd711g3',
   e2e: {
-    baseUrl: 'http://localhost:3000'
+    baseUrl: 'http://localhost:3000',
+    setupNodeEvents(on, config) {
+      config.env = process.env;
+    }
   },
   component: {
     devServer: {
