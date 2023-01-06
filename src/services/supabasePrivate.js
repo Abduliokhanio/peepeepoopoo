493d521b574921b1fb7edb604def254b52b8c4ab
Johnathan Chen <johnathan@mydocspace.com>
2023-01-06 06:10:37 -0600

    update

--- src/services/supabasePrivate.js
+++ src/services/supabasePrivate.js
@@ -1,8 +1,8 @@
 /* eslint-disable no-undef */
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
-const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL || REACT_APP_SUPABASE_URL;
+const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY || REACT_APP_SUPABASE_ANON_KEY;
 
 export const supabasePrivate = createClient(supabaseUrl, supabaseAnonKey, {
   db: {
--- src/services/supabasePublic.js
+++ src/services/supabasePublic.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-undef */
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
-const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL || REACT_APP_SUPABASE_URL;
+const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY || REACT_APP_SUPABASE_ANON_KEY;
 
 export const supabasePublic = createClient(supabaseUrl, supabaseAnonKey);
