9cd37506740577557c3ddb8f1bd0e51ccbbf6e16
Johnathan Chen <johnathan@mydocspace.com>
2023-01-06 03:50:06 -0600

    baseurl cypress config

--- cypress.config.js
+++ cypress.config.js
@@ -1,24 +1,16 @@
-const { fetchProfile, getCurrentSession } = require("./cypress/plugins/tasks");
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
-  projectId: "d711g3",
+  projectId: 'd711g3',
 
   e2e: {
-    setupNodeEvents(on, config) {
-      on("task", {
-        fetchProfile,
-        getCurrentSession,
-      });
-
-      return config;
-    },
+    baseUrl: 'http://localhost:3000'
   },
 
   component: {
     devServer: {
-      framework: "create-react-app",
-      bundler: "webpack",
+      framework: 'create-react-app',
+      bundler: 'webpack',
     },
   },
 });
