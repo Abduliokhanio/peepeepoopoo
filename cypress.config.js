6f392f0b2fcb9c9603cfbed87ea3c9e8741839e6
Johnathan Chen <johnathan@mydocspace.com>
2023-01-06 06:00:47 -0600

    udpates

--- cypress.config.js
+++ cypress.config.js
@@ -2,9 +2,6 @@ const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   projectId: 'd711g3',
-  env: {
-    PERCY_TOKEN: 'c5336ebef23a5d9ac53f7a6f30255eb10c445cd785144546eca58cc82fea8587'
-  },
   e2e: {
     baseUrl: 'http://localhost:3000'
   },
--- cypress/e2e/visual/visualTesting.cy.js
+++ cypress/e2e/visual/visualTesting.cy.js
@@ -1,19 +1,25 @@
-describe('Integration test with visual testing', function () {
-  it('Loads categories', function () {
-    cy.visit('/nu-wood-fire-grill');
-    cy.contains('Appetizers').then(() => {
-      cy.percySnapshot('categories test', {
-        widths: [375]
+describe('Integration test with visual testing',
+  {
+    env: {
+      REACT_APP_SUPABASE_URL: Cypress.env('REACT_APP_SUPABASE_URL'),
+      REACT_APP_SUPABASE_ANON_KEY: Cypress.env('REACT_APP_SUPABASE_ANON_KEY')
+    },
+  }, () => {
+    it('Loads categories', () => {
+      cy.visit('/nu-wood-fire-grill');
+      cy.contains('Appetizers').then(() => {
+        cy.percySnapshot('categories test', {
+          widths: [375]
+        });
+      });
+    });
+    it('Loads products', () => {
+      cy.visit('/products');
+      cy.contains('Appetizers').then(() => {
+        cy.percySnapshot('products test', {
+          widths: [375]
+        });
       });
     });
   });
-  it('Loads products', function () {
-    cy.visit('/products');
-    cy.contains('Appetizers').then(() => {
-      cy.percySnapshot('products test', {
-        widths: [375]
-      });
-    });
-  });
-});
 
